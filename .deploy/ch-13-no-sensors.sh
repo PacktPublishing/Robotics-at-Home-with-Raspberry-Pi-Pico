c9b57659dce0b357a57e0caf7b50a5d9664ad520
Danny Staple <danny@orionrobots.co.uk>
2022-04-03 15:21:55 +0100

    clear up dependancies list

--- .deploy/ch-13-no-sensors.sh
+++ .deploy/ch-13-no-sensors.sh
@@ -1,16 +1,9 @@
 .deploy/send-libs.sh \
-  adafruit_pioasm.mpy \
   adafruit_requests.mpy \
-  adafruit_vl53l1x.mpy \
-  adafruit_bno055.mpy \
   adafruit_wsgi \
-  adafruit_esp32spi \
-  adafruit_register
+  adafruit_esp32spi
 
 .deploy/send-it.sh \
   ch-13/2-fusing-sensors/no_imu/no_sensors.py \
   ch-13/2-fusing-sensors/no_imu/graphing.html \
-  ch-13/2-fusing-sensors/no_imu/robot.py \
-  ch-13/2-fusing-sensors/no_imu/pio_encoder.py \
-  ch-13/2-fusing-sensors/no_imu/pid.py \
   ch-13/2-fusing-sensors/no_imu/robot_wifi.py
